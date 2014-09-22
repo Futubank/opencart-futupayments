d074365e5992409a6c3a96ae60df4a360087c4b8
Maxim Oransky <maxim.oransky@gmail.com>
2014-09-22 17:55:00 +0400

    futubank_core.pphp update

--- upload/catalog/controller/payment/futubank_core.php
+++ upload/catalog/controller/payment/futubank_core.php
@@ -4,7 +4,9 @@
  * ВНИМАНИЕ! Это общая часть всех плагинов. Оригинал всегда лежит по адресу:
  * https://github.com/Futubank/futuplugins/blob/master/php/futubank_core.php
  * =========================================================================
- * Вывод формы оплаты:
+ *
+ * 1. Вывод формы оплаты
+ * ---------------------
  *
  * $ff = new FutubankForm($merchant_id, $secret_key, $is_test);
  *
@@ -12,7 +14,7 @@
  * $url = $ff->get_url();
  *
  * // значения полей формы
- * $form = compose(
+ * $form = $ff->compose(
  *     $amount,        // сумма заказа
  *     $currency,      // валюта заказа (поддерживается только "RUB")
  *     $order_id,      // номер заказа
@@ -23,13 +25,63 @@
  *     $fail_url,      // URL, куда направить клиента при ошибке
  *     $cancel_url,    // URL текущей страницы
  *     $meta,          // дополнительная информация в свободной форме (необязательно)
- *     $desctiption    // описание (необязательно)
+ *     $description,    // описание (необязательно)
+ *     // для периодических платежей:
+ *     $recurring_frequency,   // частота периодических платежей (необязательно, 'day', 'week', 'month', 'quartal', 'half-year', 'year')
+ *     $recurring_finish_date, // конечная дата периодических платежей (необязательно, дата в формате 'YYYY-MM-DD')
+ *     $recurrind_tx_id,       // для рекуррентного платежа - id первой транзакции (необязательно)
+ *     $recurring_token        // для рекуррентного платежа - токен рекуррентного платежа (необязательно)
  * );
  *
  * // далее можно самостоятельно вывести $form в виде hidden-полей,
  * // а можно воспользоваться готовым статическим методом array_to_hidden_fields:
  *
  * echo "<form action='$url' method='post'>" . FutubankForm::array_to_hidden_fields($form) . '<input type="submit"></form>';
+ *
+ *
+ * 2. Приём сообщений о выполненных транзакциях (http://yoursite.com/callback.php)
+ * -------------------------------------------------------------------------------
+ * // создаём класс обработчика транзакций, который знает всё про статусы заказов в вашей системе
+ * class MyCallbackHandler extends AbstractFutubankCallbackHandler {
+ *     // предположим, вся логика вашего плагина содержится в классе MyPlugin
+ *     private $plugin;
+ *     function __construct(MyPlugin $plugin)              { $this->plugin = $plugin; }
+ *     // определяем ключевые методы. Код методов приведён исключительно для примера
+ *     protected function get_futubank_form()              { return $this->plugin->get_futubank_form(); }
+ *     protected function load_order($order_id)            { return $this->plugin->load_order($order_id); }
+ *     protected function get_order_currency($order)       { return $order->getCurrency(); }
+ *     protected function get_order_amount($order)         { return $order->getAmount(); }
+ *     protected function is_order_completed($order)       { return $order->getStatus() == 'completed'; }
+ *     protected function mark_order_as_completed($order, array $data) {
+ *         $order->setStatus('completed');
+ *         $order->save()
+ *     }
+ *     protected function mark_order_as_error($order, array $data) {
+ *         $order->setStatus('error');
+ *         $order->save()
+ *     }
+ * }
+ *
+ * // схема ориентироваочная и зависит от архитектуры вашей CMS или фреймворка
+ * $myplugin = new MyPlugin();
+ * $h = new MyCallbackHandler($myplugin);
+ * // обрабатываем сообщение от банка, пришедшее в _POST, и если всё хорошо, отмечаем заказ как оплаченный
+ * $h->show($_POST);
+ *
+ *
+ * 3. Отправка рекуррентного платежа
+ * ---------------------------------
+ *
+ * $ff = new FutubankForm($merchant_id, $secret_key, $is_test);
+ *
+ * $result = $ff->rebill(
+ *       $amount,           // сумма заказа
+ *       $currency,         // валюта заказа (поддерживается только "RUB")
+ *       $order_id,         // номер заказа
+ *       $recurrind_tx_id,  // id транзакции, получен при первом платеже
+ *       $recurring_token,  // токен рекуррентной транзакции, получен при первом платеже
+ *       $description = ''  // описание заказа (необязательно)
+ * );
  *
  */
 class FutubankForm {
@@ -38,6 +90,7 @@ class FutubankForm {
     private $is_test;
     private $plugininfo;
     private $cmsinfo;
+    private $futugate_host;
 
     function __construct(
         $merchant_id,
@@ -49,13 +102,18 @@ class FutubankForm {
         $this->merchant_id = $merchant_id;
         $this->secret_key = $secret_key;
         $this->is_test = (bool) $is_test;
-        $this->plugininfo = $plugininfo;
+        $this->plugininfo = $plugininfo ?: 'Futuplugins/PHP v.' . phpversion();
         $this->cmsinfo = $cmsinfo;
+        $this->futugate_host = 'https://secure.futubank.com';
+        //$this->futugate_host = 'http://127.0.0.1:8000';
     }
 
     function get_url() {
-        return 'https://secure.futubank.com/pay/';
-        // return 'http://127.0.0.1:8000/pay/';
+        return $this->futugate_host . '/pay/';
+    }
+
+    function get_rebill_url() {
+        return $this->futugate_host . '/api/v1/rebill/';
     }
 
     function compose(
@@ -69,28 +127,32 @@ class FutubankForm {
         $fail_url,
         $cancel_url,
         $meta = '',
-        $description = ''
+        $description = '',
+        $recurring_frequency = '',
+        $recurring_finish_date = ''
     ) {
         if (!$description) {
             $description = "Заказ №$order_id";
         }
         $form = array(
-            'testing'        => (int) $this->is_test,
-            'merchant'       => $this->merchant_id,
-            'unix_timestamp' => time(),
-            'salt'           => $this->get_salt(32),
-            'amount'         => $amount,
-            'currency'       => $currency,
-            'description'    => $description,
-            'order_id'       => $order_id,
-            'client_email'   => $client_email,
-            'client_name'    => $client_name,
-            'client_phone'   => $client_phone,
-            'success_url'    => $success_url,
-            'fail_url'       => $fail_url,
-            'cancel_url'     => $cancel_url,
-            'meta'           => $meta,
-            'sysinfo'        => $this->get_sysinfo(),
+            'testing'               => (int) $this->is_test,
+            'merchant'              => $this->merchant_id,
+            'unix_timestamp'        => time(),
+            'salt'                  => $this->get_salt(32),
+            'amount'                => $amount,
+            'currency'              => $currency,
+            'description'           => $description,
+            'order_id'              => $order_id,
+            'client_email'          => $client_email,
+            'client_name'           => $client_name,
+            'client_phone'          => $client_phone,
+            'success_url'           => $success_url,
+            'fail_url'              => $fail_url,
+            'cancel_url'            => $cancel_url,
+            'meta'                  => $meta,
+            'sysinfo'               => $this->get_sysinfo(),
+            'recurring_frequency'   => $recurring_frequency,
+            'recurring_finish_date' => $recurring_finish_date
         );
         $form['signature'] = $this->get_signature($form);
         return $form;
@@ -130,9 +192,9 @@ class FutubankForm {
         sort($keys);
         $chunks = array();
         foreach ($keys as $k) {
-            if ($params[$k] && ($k != $key)) {
-                $v = base64_encode((string) $params[$k]);
-                $chunks[] = "$k=$v";
+            $v = (string) $params[$k];
+            if (($v !== '') && ($k != 'signature')) {
+                $chunks[] = $k . '=' . base64_encode($v);
             }
         }
         return $this->double_sha1(implode('&', $chunks));
@@ -152,6 +214,41 @@ class FutubankForm {
             $result .= $characters[rand(0, strlen($characters) - 1)];
         }
         return $result;
+    }
+
+    function rebill(
+        $amount,
+        $currency,
+        $order_id,
+        $recurrind_tx_id,
+        $recurring_token,
+        $description = ''
+    ){
+        if (!$description) {
+            $description = "Заказ №$order_id";
+        }
+        $form = array(
+            'testing'               => (int) $this->is_test,
+            'merchant'              => $this->merchant_id,
+            'unix_timestamp'        => time(),
+            'salt'                  => $this->get_salt(32),
+            'amount'                => $amount,
+            'currency'              => $currency,
+            'description'           => $description,
+            'order_id'              => $order_id,
+            'initial_transaction'   => $recurrind_tx_id,
+            'recurring_token'       => $recurring_token,
+        );
+        $form['signature'] = $this->get_signature($form);
+        $paramstr = http_build_query($form);
+        $ch = curl_init($this->get_rebill_url());
+        curl_setopt($ch, CURLOPT_USERAGENT, $this->plugininfo);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $paramstr);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result);
     }
 }
 
@@ -203,7 +300,7 @@ abstract class AbstractFutubankCallbackHandler {
             }
         } else {
             $debug_messages[] = "info: order not completed";
-            if (!$this->is_order_paid($order)) {
+            if (!$this->is_order_completed($order)) {
                 if ($this->mark_order_as_error($order, $data)) {
                     $debug_messages[] = "mark order as error";
                 } else {
@@ -222,4 +319,3 @@ abstract class AbstractFutubankCallbackHandler {
         }
     }
 }
-
